01d27575b4c624d5f8675205a0bd19949edf0a38
Sergio <sergio.miranda@medismart.cl>
2023-06-20 20:10:10 -0400

    init

---
+++ src/views/ClientesView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import RouterLink from '../components/UI/RouterLink.vue';
+import Heading from '../components/UI/Heading.vue';
+
+</script>
+<template>
+    <div>
+
+        <div class="flex justify-end">
+            <RouterLink to="agregar-cliente">
+                Agregar Cliente
+            </RouterLink>
+        </div>
+
+        <Heading>Clientes</Heading>
+
+    </div>
+</template>
+
---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import  RouterLink  from '../components/UI/RouterLink.vue';
+import Heading from '../components/UI/Heading.vue';
+
+</script>
+<template>
+    <div>
+
+        <div class="flex justify-end">
+            <RouterLink to="inicio">
+                Volver
+            </RouterLink>
+        </div>
+
+        <Heading>Agregar Cliente</Heading>
+
+    </div>
+</template>
+
